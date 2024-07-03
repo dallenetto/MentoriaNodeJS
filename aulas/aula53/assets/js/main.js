75a2749db5d2b254760ed5aed0830b82c471a85d
Letícia Dallé Netto <dalle.netto@gmail.com>
2024-07-03 18:56:28 -0300

    alterando para forEach

--- aulas/aula53/assets/js/main.js
+++ aulas/aula53/assets/js/main.js
@@ -1,21 +1,22 @@
 const elementos = [
-    {tag: 'p', texto: 'Texto da tag P'},
-    {tag: 'div', texto: 'Texto da div'},
-    {tag: 'section', texto: 'Texto da section'},
-    {tag: 'footer', texto: 'Texto do footer'}
+    {tag: 'p', texto: 'Texto da tag P 2'},
+    {tag: 'div', texto: 'Texto da div 2'},
+    {tag: 'section', texto: 'Texto da section 2'},
+    {tag: 'footer', texto: 'Texto do footer 2'}
 ];
 
 const container = document.querySelector('.container');
 const div = document.createElement('div');
 
-for( let i=0; i<elementos.length; i++){
-    let {tag, texto} = elementos[i];
+elementos.forEach((elem)=> {
+    console.log(elem);
+    let {tag, texto} = elem;
 
     let tagCriada = document.createElement(tag);
     let textoCriado = document.createTextNode(texto);
 
     tagCriada.appendChild(textoCriado);
     div.appendChild(tagCriada);
-};
+});
 
 container.appendChild(div);
